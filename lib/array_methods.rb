c77c4ec3ba707cf9332d10fb0c9e7039c9fbab81
Kimberly Bone <k_bone@uncg.edu>
2019-06-19 23:47:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,21 +5,21 @@ valley = [5,4,3,2,1,0,1,2,3,4,5]
 zig_zag = [500,4,1000,5,250]
 
 def find_element_index(array, value_to_find)
-  length = array.length 
+  array.map{}
   length.times do |index|
     puts array [index]
   end 
 end
 
 def find_max_value(array)
-  length = array.length
+  array.map{}
   length.times do |index|
     puts array [index].max 
   end 
 end
 
 def find_min_value(array)
-  length = array.length 
+  array.map{}
   length.times do |index|
     puts array [index].min 
   end 
