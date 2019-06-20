2714b8f4c067b7585a98390839f446d46f3e3b53
Kimberly Bone <k_bone@uncg.edu>
2019-06-20 00:14:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,16 +16,16 @@ end
 
 
 
-def find_max_value(array)
-  array.map{}
-  length.times do |index|
-    puts array [index].max 
-  end 
-end
+# def find_max_value(array)
+#   array.map{}
+#   length.times do |index|
+#     puts array [index].max 
+#   end 
+# end
 
-def find_min_value(array)
-  array.map{}
-  length.times do |index|
-    puts array [index].min 
-  end 
-end
+# def find_min_value(array)
+#   array.map{}
+#   length.times do |index|
+#     puts array [index].min 
+#   end 
+# end
